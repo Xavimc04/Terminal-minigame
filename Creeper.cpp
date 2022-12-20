c0effb525f1823989c0cd97b2a2db47f7c8ad153
Xavier Morell <97970292+Xavimc04@users.noreply.github.com>
2022-12-20 12:05:41 +0100

    Update Creeper.cpp

--- Creeper.cpp
+++ Creeper.cpp
@@ -1,235 +1,152 @@
 
-
 #include <iostream> 
 #include <stdlib.h>
-#include <vector>
-#include <string>
+#include <vector> 
+#include <string> 
 #include <thread>        
-#include <chrono>   
-#include <windows.h>
-#include <locale.h>
+#include <chrono>  
 
 using namespace std;
 
+// @Declarations
 struct IAttack {
-    string name; 
-    int damage; 
-    bool infinite; 
-    int usages; 
+    string name;
+    int damage;
+    bool infinite;
+    int usages;
 };
 
 static std::vector<IAttack> characterAttacks = {
     { "Sencillo", 5, true, 0 },
     { "Combo", 70, false, 20 },
     { "RPG", 200, false, 5 },
+};
+
+struct IEnemies {
+    string name; 
+    int health;  
+};
+
+static std::vector<IEnemies> enemies = {
+    { "Frescura", 100 },
+    { "Reventao", 100 },
 }; 
 
-class Character {
-    public: 
-        int health;
-        bool isEnemy; 
-        string name; 
-
-        Character(int health, bool isEnemy, string name);
-
-        bool isAlive();  
-
-        void setHealth(int health); 
-}; 
-
-void Character::setHealth(int health) {
-    this->health = health;
-
-    if(this->health <= 0){
-        cout << this->name << " ha muerto..." << endl; 
-    }
-}
-
-Character::Character(int health, bool isEnemy, string name) {
-    this->health = health; 
-    this->isEnemy = isEnemy; 
-    this->name = name; 
-}
-
-bool Character::isAlive() {
-    if (this->health > 0) return true; 
-       
-    return false; 
-}
+string heroName = "Xavier"; 
+int heroHealth = 200; 
 
 string playerInput;
 
-// Centered: https://stackoverflow.com/questions/45582095/c-how-can-i-center-text-output; 
-void CoutCentered(std::string text) {
-
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    PCONSOLE_SCREEN_BUFFER_INFO lpScreenInfo = new CONSOLE_SCREEN_BUFFER_INFO();
-    GetConsoleScreenBufferInfo(hConsole, lpScreenInfo);
-    COORD NewSBSize = lpScreenInfo->dwSize;
-
-    if (NewSBSize.X > text.size()) {
-        int newpos = ((NewSBSize.X - text.size()) / 2);
-        for (int i = 0; i < newpos; i++) std::cout << " ";
-    }
-
-    std::cout << text << std::endl;
-}
-
-void validateEnemyName(string firstValue, int firstHealth, string secondValue, int secondHealth) {
-    cout << "Quieres golpear a " << firstValue << " o " << secondValue << "? ";
-    cin >> playerInput; 
-
-    bool alive = true; 
-
-    if (playerInput == firstValue) {
-        if (firstHealth > 0) {
-            return; 
-        } 
-
-        alive = false; 
-    } else if (playerInput == secondValue) {
-        if (secondHealth > 0) {
-            return; 
+// @Functions
+bool anyEnemyAlive() { 
+    for (int index = 0; index < enemies.size(); index++) {
+        if (enemies[index].health > 0) {
+            return true; 
         }
-
-        alive = false; 
-    } 
-
-    system("CLS");
-
-    if (alive) {
-        cout << "Valor introducido no válido, por favor, repite de nuevo..."; 
     }
-    else {
-        cout << "El personaje que has seleccionado golpear se encuentra muerto, por favor, selecciona otro";
-    }
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    system("CLS");
-    return validateEnemyName(firstValue, firstHealth, secondValue, secondHealth);
+    
+    return false; 
 }
 
-int selectAttack(string name) {
-    string question = "Ataques disponibles: \n\n";
+string getFormattedEnemies() {
+    string formattedString = "";
 
-    // Unsigned deg if u want to know: https://es.stackoverflow.com/questions/396050/cu%C3%A1l-es-el-significado-de-unsigned-en-c; 
+    for (unsigned int index = 0; index < enemies.size(); index++) {
+        formattedString = formattedString + "[" + to_string(index) + "] " + enemies[index].name + " - " + to_string(enemies[index].health) + "HP\n";
+    }
 
-    for (unsigned int i = 0; i < characterAttacks.size(); i++) {
-        question = question + "- " + characterAttacks[i].name + ": " + (
-            (characterAttacks[i].usages > 0) ?
-                std::to_string(characterAttacks[i].usages)
-            : 
-                ((characterAttacks[i].infinite == true) ? "Infinito" : "Agotado")    
-        ) + "\n";
-    }  
+    return formattedString; 
+}
 
-    cout << question << "\nIntroduce nombre de ataque: ";
+int validateEnemyValues() {
     cin >> playerInput;
 
-    bool founded = false; 
+    for (unsigned int index = 0; index < enemies.size(); index++) {
+        if (enemies[index].name == playerInput) {
+            if (enemies[index].health > 0) {
+                return index; 
+            }
+            else {
+                cout << "Este enemigo esta muerto, introduce otro nombre: ";
+                return validateEnemyValues();
+            }
+        }
+    }
+    
+    cout << "Nombre no valido, por favor, introduce de nuevo: "; 
+    return validateEnemyValues();
+}
 
+void attackSomeone(string attackerName, int &defenderHealth, int damage) {
+    cout << attackerName << " acaba de atacar y ha hecho " << damage << " de daÃ±o.\n"; 
+    defenderHealth = defenderHealth - damage; 
+
+    if (defenderHealth < 0) {
+        defenderHealth = 0; 
+    }
+
+    return; 
+}
+
+int selectAttack() {
     system("CLS");
+    string question = "Ataques disponibles: \n\n";
 
     for (unsigned int i = 0; i < characterAttacks.size(); i++) {
-        if (playerInput == characterAttacks[i].name) { 
-            if (characterAttacks[i].infinite) {
-                founded = true;
-                cout << "Has usado el ataque \033[31m" << characterAttacks[i].name << "\033[0m y has producido " << characterAttacks[i].damage << " a " << name << endl;
-                return characterAttacks[i].damage;
-                break;
-            } 
+        question = question + "[" + characterAttacks[i].name + "] Usos: " + (
+            (characterAttacks[i].usages > 0) ?
+                std::to_string(characterAttacks[i].usages)
+            :
+                ((characterAttacks[i].infinite == true) ? "Infinito" : "Agotado")
+        ) + "\n";
+    }
+
+    question = question + "\nIntroduce nombre de ataque: "; 
+
+    cout << question; 
+    cin >> playerInput;  
+
+    for (unsigned int i = 0; i < characterAttacks.size(); i++) {
+        if (playerInput == characterAttacks[i].name) {
+            if (characterAttacks[i].infinite) { 
+                return characterAttacks[i].damage; 
+            }
 
             if (characterAttacks[i].usages > 0) {
                 characterAttacks[i].usages--; 
-                founded = true;
-                cout << "Has usado el ataque \033[31m" << characterAttacks[i].name << "\033[0m y has producido " << characterAttacks[i].damage << " a " << name << endl;
-                return characterAttacks[i].damage;
-                break;
-            } 
+                return characterAttacks[i].damage; 
+            }
         }
     }
 
-    if (!founded) {
-        system("CLS");
-
-        cout << "Valor introducido no válido, por favor, repite de nuevo...";
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-
-        system("CLS");
-
-        return selectAttack(name);
-    }
+    cout << "Valor introducido no vÃ¡lido, por favor, repite de nuevo..." << endl;
+    return selectAttack();
 }
 
+// @Main thread
 int main()
 {
+    while (heroHealth > 0 && anyEnemyAlive()) {
+        cout << heroName << " tienes el turno de ataque, selecciona tu contrincante \n\n" << getFormattedEnemies() << "\nIntroduce el nombre: ";
+        int toAttackId = validateEnemyValues(); 
+        int attackDamage = selectAttack(); 
 
-    setlocale(LC_ALL, "spanish");
-    system("color 00"); 
-    system("CLS");
+        system("CLS");
+ 
+        attackSomeone(heroName, enemies[toAttackId].health, attackDamage);
 
-    cout << "Bienvenido, por favor, selecciona un nombre de usuario: ";
-    cin >> playerInput; 
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        system("CLS");
 
-    Character playerSoldier(400, false, playerInput); 
-
-    Character firstEnemy(200, true, "Creeper");
-    Character secondEnemy(200, true, "Boss");
-
-    system("CLS");
-
-    cout << playerSoldier.name << " parece que te has metido en un buen lio eh ¿Vas a querer pelear? [y/n] : ";
-    cin >> playerInput; 
-
-    system("CLS");
-
-    if (playerInput == "y") {
-        while (playerSoldier.isAlive() && firstEnemy.isAlive() || secondEnemy.isAlive()) {
-            validateEnemyName(firstEnemy.name, firstEnemy.health, secondEnemy.name, secondEnemy.health);
-
-            system("CLS");
-
-            if (playerInput == firstEnemy.name) {
-                int attackedDamage = selectAttack(firstEnemy.name); 
-                firstEnemy.setHealth(firstEnemy.health - attackedDamage); 
-            } 
-            else {
-                int attackedDamage = selectAttack(secondEnemy.name);
-                secondEnemy.setHealth(secondEnemy.health - attackedDamage);
-            }
-
-            system("CLS");
-
-            int firstEnemyAttack = rand() % 50 + 2;
-            int secondEnemyAttack = rand() % 50 + 2;
-
-            if (firstEnemy.isAlive()) {
-                playerSoldier.setHealth(playerSoldier.health - firstEnemyAttack);
-                cout << firstEnemy.name << " te ha atacado y has sufrido \033[31m" << firstEnemyAttack << "\033[0m de daño, te quedan: \033[32m" << playerSoldier.health << "\033[0mHP" << endl;
-            }
-
-            if (secondEnemy.isAlive()) {
-                playerSoldier.setHealth(playerSoldier.health - secondEnemyAttack);
-                cout << secondEnemy.name << " te ha atacado y has sufrido \033[31m" << secondEnemyAttack << "\033[0m de daño, te quedan: \033[32m" << playerSoldier.health << "\033[0mHP" << endl;
-            } 
-
-            std::this_thread::sleep_for(std::chrono::seconds(7));
-
-            system("CLS");
+        for (unsigned int index = 0; index < enemies.size(); index++) {
+            if (enemies[index].health > 0) {
+                attackSomeone(enemies[index].name, heroHealth, (rand() % 50 + 2)); 
+            }  
         }
 
-        if (playerSoldier.isAlive()) {
-            system("color 27");
-            CoutCentered("Has ganado"); 
-        }
-        else {
-            system("color 47");
-            CoutCentered("Has perdido");  
-        }
-
-        std::this_thread::sleep_for(std::chrono::seconds(10));
-    } 
-    else {
-        cout << playerSoldier.name << " una retirada a tiempo es una victoria no? o eso dicen... " << endl; 
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        system("CLS");
     }
+
+    cout << "El juego ha terminado"; 
 }
